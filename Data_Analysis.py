5f15942e3211cbca2a9f70fc7c57eb8f38abd417
Vitor Cardoso <vitorcsantos.github@gmail.com>
2026-01-28 08:53:55 -0300

    Importing Pandas - Importing Data Frame - Creating Jupyter notebook

--- Data_Analysis.py
+++ Data_Analysis.py
@@ -1,1 +1,7 @@
-print ('Hello World!')
+import pandas as pd
+
+#Import data archive
+#Use data .read_[Type]("Link")
+df = pd.read_csv("https://raw.githubusercontent.com/guilhermeonrails/data-jobs/refs/heads/main/salaries.csv")
+
+df.head()
